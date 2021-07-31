65cc8af4f0c8fcf381535a7d0d79edfc5e721885
taner <taner@DESKTOP-8FBSMG9>
2021-07-31 11:23:34 +0300

    Add project files.

---
+++ Generic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Taner");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Haşim");
+            Console.WriteLine(isimler.Length);
+            foreach(var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
